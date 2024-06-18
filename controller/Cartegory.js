634e1252de400d1a9096d6601ad9414b6fac4f24
nakulpaul07 <nakulpal105@gmail.com>
2024-06-18 13:43:08 +0530

    Comment Cartegory

--- controller/Cartegory.js
+++ controller/Cartegory.js
@@ -1,64 +1,64 @@
-const CartegoryModel = require('../model/cartegory')
-const cloudinary = require('cloudinary').v2;
+// const CartegoryModel = require('../model/cartegory')
+// const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({
-    cloud_name: 'dqqgdxtgx',
-    api_key: '661387327716212',
-    api_secret: 'aOQnvPghucWnXASKpR6AZu_i93Y'
-});
+// cloudinary.config({
+//     cloud_name: 'dqqgdxtgx',
+//     api_key: '661387327716212',
+//     api_secret: 'aOQnvPghucWnXASKpR6AZu_i93Y'
+// });
 
-class cartegoryController {
+// class cartegoryController {
 
-    static createCategory = async(req,res) =>{
-        try {
-            console.log(req.body)
-            console.log(req.files.image)
+//     static createCategory = async(req,res) =>{
+//         try {
+//             console.log(req.body)
+//             console.log(req.files.image)
 
-            const file = req.files.image
-            const uploadImage = await cloudinary.uploader.upload(file.tempFilePath,{
-                folder: 'profileapiimage'
-            })
+//             const file = req.files.image
+//             const uploadImage = await cloudinary.uploader.upload(file.tempFilePath,{
+//                 folder: 'profileapiimage'
+//             })
 
-            const {name} = req.body
-            const data = new CartegoryModel({
-                name:name,
-                image: {
-                    public_id: uploadImage.public_id,
-                    url: uploadImage.secure_url,
-                },
-            })
-            await data.save()
-            res.status(201).json({ status: "success", message: "category added successfully ✔✔" })
+//             const {name} = req.body
+//             const data = new CartegoryModel({
+//                 name:name,
+//                 image: {
+//                     public_id: uploadImage.public_id,
+//                     url: uploadImage.secure_url,
+//                 },
+//             })
+//             await data.save()
+//             res.status(201).json({ status: "success", message: "category added successfully ✔✔" })
 
             
-        } catch (error) {
-            console.log(error)
+//         } catch (error) {
+//             console.log(error)
             
-        }
-    }
+//         }
+//     }
 
-    static getAllCategories = async (req,res) =>{
-        try {
-            const allCategories = await CartegoryModel.find()
-            res.status(200).json({success:true,allCategories})
+//     static getAllCategories = async (req,res) =>{
+//         try {
+//             const allCategories = await CartegoryModel.find()
+//             res.status(200).json({success:true,allCategories})
             
-        } catch (error) {
-            console.log(error)
+//         } catch (error) {
+//             console.log(error)
 
-        }
-    }
+//         }
+//     }
 
-    static getCategoryDetail = async (req,res) =>{
-        try {
-            const CategoryDetail = await CartegoryModel.findById(req.params.id)
-            res.status(200).json({success:true,CategoryDetail})
+//     static getCategoryDetail = async (req,res) =>{
+//         try {
+//             const CategoryDetail = await CartegoryModel.findById(req.params.id)
+//             res.status(200).json({success:true,CategoryDetail})
             
-        } catch (error) {
-            console.log(error)
+//         } catch (error) {
+//             console.log(error)
 
-        }
-    }
+//         }
+//     }
 
-}
+// }
 
-module.exports = cartegoryController
+// module.exports = cartegoryController
--- model/cartegory.js
+++ model/cartegory.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const CartegorySchema = new mongoose.Schema(
-    {
-    name: {
-        type: String,
-        Required: true
-    },
-    image: {
+// const CartegorySchema = new mongoose.Schema(
+//     {
+//     name: {
+//         type: String,
+//         Required: true
+//     },
+//     image: {
 
-        public_id: {
-            type: String,
-            Required: true,
-        },
+//         public_id: {
+//             type: String,
+//             Required: true,
+//         },
 
-        url: {
-            type: String,
-            Required: true,
-        },
+//         url: {
+//             type: String,
+//             Required: true,
+//         },
 
-    },
+//     },
    
-}, { timestamps: true })
-const CartegoryModel = mongoose.model('Cartegories', CartegorySchema)
+// }, { timestamps: true })
+// const CartegoryModel = mongoose.model('Cartegories', CartegorySchema)
 
-module.exports = CartegoryModel
+// module.exports = CartegoryModel
